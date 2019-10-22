df4a2b849f52dcb0094f01a2399f535a5b20ef6e
sainsh <kfsainsh@gmail.com>
2019-10-22 21:06:21 +0200

    formatting

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const app = express()
 const port = 1337
 
 
-app.post('/loginY', (req, res) =>
-console.log(`user ${req.user} tried to log on`)
-if(auth(req.user, req.pass)) {
-res.send()
-} else{
-res.send("wrong username or password") 
-} 
- )
+app.post('/loginY', (req, res) => {
+    console.log(`user ${req.user} tried to log on`)
+    if (auth(req.user, req.pass)) {
+        res.send()
+    } else {
+        res.send("wrong username or password")
+    }
+})
 
-auth(user, pass) 
+function auth(user, pass) {
+    if (db.getUser(user, pass)) {
+        return true;
+    }
+
+    return false;
+}
