ad1ea80e489e7b6ca14532ad30d5b1116fc22e84
sainsh <kfsainsh@gmail.com>
2019-10-23 11:58:07 +0200

    added oel to ul as li

--- routes/oel.js
+++ routes/oel.js
@@ -7,7 +7,10 @@ router.get('/', function(req, res, next) {
         {'navn': 'hobgoblin', 'smag': 'rugbrød', 'vol' : '5.0'},
         {'navn': 'black wych', 'smag': 'rugbrød plus bær', 'vol' : '5.4'},
         {'navn': 'gingerbeard', 'smag': 'ingefær', 'vol' : '5.0'} ];
-  res.render('index', { title: 'Øl', oel: oel });
+
+  res.render('oel', { title: 'Øl', oel1: ` navn: ${oel[0].navn}, smag: ${oel[0].smag}, vol: ${oel[0].vol}` ,
+   oel2:` navn: ${oel[1].navn}, smag: ${oel[1].smag}, vol: ${oel[1].vol}`,
+    oel3: ` navn: ${oel[2].navn}, smag: ${oel[2].smag}, vol: ${oel[2].vol}`});
 });
 
 
