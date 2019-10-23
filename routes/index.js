bfc102f19c7ffc129758e6821a786df0b5e94aeb
sainsh <kfsainsh@gmail.com>
2019-10-23 11:02:20 +0200

    added /users route changed besked p to h2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', besked: 'Det er en god dag!' });
 });
 
+router.get('/users', function(req,res, next){
+  res.send(JSON.stringify("[{name: 'thomas', pass: '1234' },{name: 'thomas2', pass: '12342' }, {name: 'thomas3', pass: '12343' }]"))
+});
+
 module.exports = router;
