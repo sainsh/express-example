e8ebc3a1bcd40c8377ff867cab848a5ac8420c00
sainsh <kfsainsh@gmail.com>
2019-10-23 11:18:48 +0200

    started working on øl assignment

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', besked: 'Det er en god dag!' });
+  res.render('index', { title: 'Øl-siden', besked: 'Det er en god dag til en øl!' });
 });
 
-router.get('/users', function(req,res, next){
-  res.send(JSON.stringify("[{name: 'thomas', pass: '1234' },{name: 'thomas2', pass: '12342' }, {name: 'thomas3', pass: '12343' }]"))
-});
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var users = [{'name': 'thomas', 'pass': '1234'},{'name': 'thomas2', 'pass': '12342' }, {'name': 'thomas3', 'pass': '12343' }]
+  res.send(users);
 });
 
 module.exports = router;
