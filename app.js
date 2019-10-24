795d35bf0a528a2c38e08a7f09f97117dfe6e238
sainsh <kfsainsh@gmail.com>
2019-10-24 16:09:58 +0200

    added API functionality

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var oelRouter = require('./routes/oel');
 var omosRouter = require('./routes/om-os')
+var beersRouter = require('./routes/beers')
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/oel', oelRouter);
 app.use('/om-os', omosRouter);
+app.use('/api/beers', beersRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/beers.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+router.get('/', function (req, res, next) {
+    var beers = JSON.parse(fs.readFileSync('public/json/oel.json'))
+    res.send(beers)
+});
+
+router.post('/', function (req, res, next) {
+
+    var beer = req.body;
+
+    fs.readFile('public/json/oel.json', function (err, data) {
+
+        var json = JSON.parse(data);
+        json.push(beer)
+        fs.writeFileSync('public/json/oel.json', JSON.stringify(json))
+        res.send(beer)
+    })
+});
+
+router.get('/:id', function (req, res, next) {
+    var beers = JSON.parse(fs.readFileSync('public/json/oel.json'));
+    var id = req.path.slice(1);
+    if (id > beers.length) {
+        res.status(404).send(`could not find the beer you're looking for`)
+    } else {
+
+    res.send(beers[id]);
+    }
+})
+
+router.put('/:id', function (req, res, next) {
+    var id = req.path.slice(1)
+   
+
+    var beers = JSON.parse(fs.readFileSync('public/json/oel.json'));
+    if (id > beers.length) {
+        res.status(404).send(`could not find the beer you're looking for`)
+    } else {
+        var beer = req.body
+        beers[id] = beer
+
+
+        fs.writeFileSync('public/json/oel.json', JSON.stringify(beers))
+        res.send(beer)
+    }
+})
+
+router.delete('/:id', function(req, res, next){
+    var id = req.path.slice(1)
+    var beers = JSON.parse(fs.readFileSync('public/json/oel.json'));
+
+    if (id > beers.length) {
+        res.status(404).send(`could not find the beer you're looking for`)
+    } else {
+        var beer = beers[id];
+        beers = beers.filter(function(value, index, arr){
+            return index != id;
+        })
+
+        fs.writeFileSync('public/json/oel.json', JSON.stringify(beers))
+        res.send(beer)
+    }
+
+})
+
+
+
+
+module.exports = router;
